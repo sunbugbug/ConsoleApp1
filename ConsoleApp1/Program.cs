8f488bc0933fe3993cd16e85db4df8f7e7b34efb
unknown <Yang@Yang-PC.lgcns.com>
2019-04-09 09:46:22 +0900

    초기 커밋.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Net.Sockets;
+using System.Net;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IPAddress[] addresslist = Dns.GetHostAddresses("192.168.137.240");
+
+            Socket transferSock = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.IP);
+            transferSock.Connect(new IPEndPoint(addresslist[0], 1234));
+
+            lightEntities le = new lightEntities();
+            Light light = new Light();
+
+            //byte[] transferStr = Encoding.Default.GetBytes("Hi");
+            //transferSock.Send(transferStr);
+
+            //byte[] receivedStr = new byte[11];
+
+            //transferSock.Receive(receivedStr);
+            //Console.WriteLine(Encoding.Default.GetString(receivedStr));
+
+            while (true)
+            {
+                //Random r = new Random();
+
+                //int rnd = r.Next(1, 1001);
+
+                //byte[] transferInt = Encoding.Default.GetBytes(String.Format("{0}","a"));
+                //transferSock.Send(transferInt);
+
+                byte[] receivedStr = new byte[10];
+
+                transferSock.Receive(receivedStr);
+
+                Console.WriteLine(Encoding.Default.GetString(receivedStr));
+            }
+        }
+    }
+}
